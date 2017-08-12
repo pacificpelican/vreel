f37db0a8e28d0dd8274ab6f9f7b81c1b86bbcc2c
Daniel McKeown <2014@pacificpelican.us>
2017-08-11 21:26:02 -0700

    bugfix package.json script and logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,21 +6,20 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1><a href="../">vreel App</a></h1>
-				<div id="logo">
+				<span id="logo">
 					<a href="http://vreel.pacificio.com">
-						<svg id="vReelLogo">
-							<rect x="30" y="4" width="241" height="10"/>
-							<circle id="firstCircle" cx="40" cy="32" r="30"/>
-							<circle id="firstCircleInner" cx="40" cy="33" r="20"/>
-							<circle id="secondCircle" cx="260" cy="32" r="30"/>
-							<circle id="secondCircleInner" cx="260" cy="33" r="20"/>
-							<text x="95" y="55" font-family="Hack, Menlo, mono" font-size="35">
+						<svg id="vReelLogo" alt="vreel">
+							<rect x="30" y="6" width="141" height="4"/>
+							<circle id="firstCircle" cx="40" cy="23" r="15"/>
+							<circle id="firstCircleInner" cx="40" cy="24" r="10"/>
+							<circle id="secondCircle" cx="160" cy="23" r="15"/>
+							<circle id="secondCircleInner" cx="160" cy="24" r="10"/>
+							<text x="62" y="35" font-family="Hack, Menlo, mono" font-size="23">
 								vReel
 							</text>
 						</svg>
 					</a>
-				</div>
+				</span>
 				<nav>
 					<a href="http://vreel.pacificio.com">Project Home</a>
 					<Link href="/profile">Me</Link>
